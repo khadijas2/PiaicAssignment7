7ad88ddb410d35cbc9d08656351217ae5ee1203a
Khadija Sikander <107493773+khadijas2@users.noreply.github.com>
2023-08-11 01:30:18 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,18 @@
+// //Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+// Addition
+let additionResult = 5 + 3;
+console.log("Addition:", additionResult);
+
+// Subtraction
+let subtractionResult = 15 - 7;
+console.log("Subtraction:", subtractionResult);
+
+// Multiplication
+let multiplicationResult = 4 * 2;
+console.log("Multiplication:", multiplicationResult);
+
+// Division
+let divisionResult = 32 / 4;
+console.log("Division:", divisionResult);
+
